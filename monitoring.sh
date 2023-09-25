e64d7ae69bfff4e005ab95253555dbfc904f4835
Mr. Norkum <kelestayyib@gmail.com>
2023-09-25 05:20:22 +0300

    Successful

--- monitoring.sh
+++ monitoring.sh
@@ -1,35 +1,30 @@
 #!/bin/bash
-
 arc=$(uname -a)
 p_cpu=$(nproc)
 v_cpu=$(getconf _NPROCESSORS_ONLN)
-usage_ram=$(free -m | awk '/Mem/ {print $3}')
-total_ram=$(free -m | awk '/Mem/ {print $2}')
-p_ram=$(free | awk '/Mem/ {printf("%.2f", $3/$2*100)}')
+mem_usage=$(free -m | grep 'Mem' | awk '{printf("%d/%dMB(%.2f%%)", $3,$2, $3/$2 * 100)}')
 usage_disk=$(df --total -BM | awk '/total/ {print $3}')
 total_disk=$(df --total -h | awk '/total/ {print $2}')
 p_disk=$(df --total | awk '/total/ {print $5}')
-last_boot_day=$(who -b | awk '{print $3}')
-last_boot_time=$(who -b | awk '{print $4}')
-lv_count=$(vgdisplay | awk '/Open LV/ {print $3}')
-lv_status=$(if [ $lv_count -eq 0 ]; then echo no; else echo yes; fi)
-users_log=$(users | wc -w)
-cpu_load=$(top -bn1 | grep '^%Cpu' | cut -c 9- | xargs | awk '{printf("%.1f%%"), $1 + $3}')
-ip=$(hostname -I)
-ctcp=$(cat /proc/net/sockstat{,6} | awk '$1 == "TCP:" {print $3}')
-mac=$(ip link show | awk '$1 == "link/ether" {print $2}')
-cmds=$(journalctl _COMM=sudo | grep COMMAND | wc -l)
+cpu_load=$(top -bn1 | grep '^%Cpu' | awk '{printf "%.1f%%", $2 + $4}')
+last_boot=$(who -b | awk '{print $3, $4}')
+lvm_use=$(lsblk | grep 'LVM' | wc -l | awk '{if($1>0) print "yes"; else print "no"}')
+tcp=$(cat /proc/net/sockstat | grep 'TCP' | awk '{print $3}')
+users=$(users | wc -l)
+ipv4=$(hostname -I)
+mac=$(ip link | grep 'link/ether' | awk '{print $2}')
+sudo=$(cat /var/log/sudo/sudo.log | wc -l | tr '\n' ' ')
 wall << EOF
-	#Architecture : $arc
-	#CPU physical : $p_cpu
-	#vCPU : $v_cpu
-	#Memory Usage : $usage_ram/${total_ram}MB ($p_ram%)
-	#Disk Usage : $usage_disk/$total_disk ($p_disk)
-	#CPU load : $cpu_load
-	#Last boot : $last_boot_day $last_boot_time
-	#LVM use : $lv_status
-	#Connections TCP : $ctcp ESTABLISHED
-	#User log : $users_log
-	#Network : IP $ip ($mac)
-	#Sudo: $cmds cmd
+	#Architecture: $arc
+	#CPU physical: $p_cpu
+	#vCPU: $v_cpu
+	#Memory Usage:$mem_usage
+	#Disk Usage: $usage_disk/$total_disk ($p_disk)
+	#CPU load:$cpu_load
+	#Last boot:$last_boot
+	#LVM use:$lvm_use
+	#Connections TCP:$tcp ESTABLISHED
+	#User log:$users
+	#Network:IP $ipv4($mac)
+	#Sudo: $sudo cmd
 EOF
